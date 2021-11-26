5049e3c45cb818ab2ebd43c5e246b4df3997dac5
Christian Mendes Pereira <christianp3m@gmail.com>
2021-11-26 18:51:48 -0300

    Part 8: if there is an even betwenn three numbers

---
+++ fundamentos/dia-1-javascript-primeiros-passos/script8.js
@@ -0,0 +1,12 @@
+let num1 = 3;
+let num2 = 7;
+let num3 = 1;
+
+isEven1 = num1 % 2 === 0;
+isEven2 = num2 % 2 === 0;
+isEven3 = num3 % 2 === 0;
+
+result = isEven1 || isEven2 || isEven3;
+
+console.log(result);
+
