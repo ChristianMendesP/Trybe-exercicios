7176b23758eacb6ee4fcd5fcecef06e76e9750cc
Christian Mendes Pereira <christianp3m@gmail.com>
2021-11-26 19:46:24 -0300

    Finalizating

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -0,0 +1,21 @@
+let estado = "lista";
+
+switch (estado) {
+    estado = "nao sei";
+    case "aprovada":
+        console.log("aprovada");
+        break;
+
+    case "reprovada":
+        console.log("reprovada");
+        break;
+
+    case "lista":
+        console.log("lista");
+        break;
+        
+    default:
+        console.log("nao se aplica");
+}   
+
+    
