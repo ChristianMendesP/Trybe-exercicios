9e9a4b053f04b40093c375259893a067f6934e27
Christian Mendes Pereira <christianp3m@gmail.com>
2021-11-26 18:46:07 -0300

    Part7: Score converter

---
+++ fundamentos/dia-1-javascript-primeiros-passos/script7.js
@@ -0,0 +1,18 @@
+let value = 50;
+let result;
+
+if (value < 50) {
+    result = "F";
+} else if (value < 60) {
+    result = "E";
+} else if (value < 70) {
+    result = "D";
+} else if (value < 80) {
+    result = "C";
+} else if (value < 90) {
+    result = "B";
+} else if (value <= 100) {
+    result = "A";
+} 
+
+console.log(result);
