d357058eadb613e2a0c46a808033e242861b6bad
Christian Mendes Pereira <christianp3m@gmail.com>
2021-11-30 18:04:03 -0300

    Triangle of *

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/script2.js
@@ -0,0 +1,10 @@
+let n = 5;
+let array = "";
+
+for (let row=1; row<=n; row+=1) {
+    for (let col=1; col<=row; col+=1) {
+        array = array + "*";
+    }
+    console.log(array);
+    array = "";
+}
