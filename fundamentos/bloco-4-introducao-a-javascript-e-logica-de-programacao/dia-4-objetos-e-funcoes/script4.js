e7f266f4de6ad7fb565d816a21600af25c70ad70
Christian Mendes Pereira <christianp3m@gmail.com>
2021-12-01 18:07:28 -0300

    Printing values of 2 objects together

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/script4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/script4.js
@@ -2,9 +2,26 @@ let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'Sim'
-}
+  recorrente: 'Nao'
+};
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Nao'
+};
 
 for (let index in info) {
-  console.log(info[index]);
-}
+  if (index != 'recorrente') {
+    console.log(info[index] + ' e ' + info2[index]);
+  } else {
+    if (info[index] === info2[index] && info2[index] === 'Sim') {
+      console.log('Ambos recorrentes');
+    } else if (info[index] === info2[index] && info2[index] === 'Nao'){
+      console.log('Ambos nao recorrentes');
+    } else {
+      console.log(info[index] + ' e ' + info2[index]);
+    }
+  }
+};
