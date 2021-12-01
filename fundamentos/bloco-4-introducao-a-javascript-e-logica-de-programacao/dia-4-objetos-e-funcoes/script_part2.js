c5d5c2b8377798ba764879f58ca95229c8f5ab2d
Christian Mendes Pereira <christianp3m@gmail.com>
2021-12-01 18:39:43 -0300

    Function for determine the smallest number

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/script_part2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/script_part2.js
@@ -1,19 +1,35 @@
 let arrayNumbers = [2, 3, 6, 7, 10, 1];
 
+let result = theSmallest(arrayNumbers);
+
+console.log( result );
+
+function theSmallest(array) {
+  let indexSmallest = 0;
+  for (let index = 1; index < array.length; index += 1) {
+    if (array[index] < array[indexSmallest]) {
+      indexSmallest = index;
+    }
+  }
+  return(indexSmallest);
+}
+
+
+/* let arrayNumbers = [2, 3, 6, 7, 10, 1];
+
 let result = theGreatest(arrayNumbers);
 
 console.log( result );
 
 function theGreatest(array) {
-  let greatest = array[0];
+  let indexGreatest = 0;
   for (let index = 1; index < array.length; index += 1) {
-    if (array[index] > greatest) {
-      greatest = array[index];
+    if (array[index] > array[indexGreatest]) {
+      indexGreatest = index;
     }
   }
-  return(greatest);
-}
-
+  return(indexGreatest);
+} */
 
 
 /* let word = "carro";
