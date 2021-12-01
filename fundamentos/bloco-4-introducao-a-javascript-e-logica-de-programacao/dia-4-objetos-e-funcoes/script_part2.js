22ccb1478bd90916622de48362a47cadea2bdcb4
Christian Mendes Pereira <christianp3m@gmail.com>
2021-12-01 18:36:35 -0300

    Function for determine the greatest number

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/script_part2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/script_part2.js
@@ -1,4 +1,22 @@
-let word = "carro";
+let arrayNumbers = [2, 3, 6, 7, 10, 1];
+
+let result = theGreatest(arrayNumbers);
+
+console.log( result );
+
+function theGreatest(array) {
+  let greatest = array[0];
+  for (let index = 1; index < array.length; index += 1) {
+    if (array[index] > greatest) {
+      greatest = array[index];
+    }
+  }
+  return(greatest);
+}
+
+
+
+/* let word = "carro";
 
 let result = verificaPalindrome(word);
 
@@ -14,4 +32,4 @@ function verificaPalindrome(word) {
     }
   }
   return(isPalindrom);
-}
+} */
