4fa649bba9453e97dc4f5189d384596559e250b1
Christian Mendes Pereira <christianp3m@gmail.com>
2022-01-05 00:05:24 -0300

    Handling with regular expressions in js

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/script.js
@@ -1,3 +1,5 @@
+// Add option to the list of states
+
 let allStates = [
   ['Acre', 'AC'],
   ['Alagoas', 'AL'],
@@ -45,7 +47,67 @@ function addOption(states) {
 
 addOption(allStates);
 
+// preventDefault to the form
+
 const submitButton = document.querySelector('#submit-button-id');
 submitButton.addEventListener('click', (event) => {
-  event.preventDefault();
+  //event.preventDefault();
 });
+
+// Date validation
+submitButton.addEventListener('click', () => {
+  let date = document.querySelector('#date-begin-id');
+  let text = date.value;
+  console.log(text);
+  let pattern = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+
+  if (text.length === 10) {
+    if (!pattern.test(text)) {
+      alert('Padrão de data incorreto')
+    }
+  }
+});
+
+
+// Limitation of number of character
+function limitChar(element, maxnumber) {
+  if (element.value.length > maxnumber - 1) {
+    element.value = element.value.substring(0,maxnumber);
+  }
+}
+
+// Validation for full name
+let fullName = document.querySelector('#name-id');
+fullName.pattern = "^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,5}$";
+fullName.addEventListener('keyup', () => {
+  limitChar(fullName, 40);
+});
+
+// Validation for cpf
+let cpf = document.querySelector('#cpf-id');
+cpf.pattern = "[0-9]{11}";
+cpf.addEventListener('keyup', () => {
+  limitChar(cpf, 11);
+});
+
+// Validation for city
+let city = document.querySelector('#city-id');
+city.pattern = "^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,5}$";
+city.addEventListener('keyup', () => {
+  limitChar(city, 28);
+});
+
+// Validation for adress
+let adress = document.querySelector('#adress-id');
+adress.addEventListener('keyup', ()=> {
+  limitChar(adress, 500);
+});
+
+// Validation for date
+let date = document.querySelector('#date-id');
+date.pattern = "/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i";
+date.addEventListener('keyup', ()=> {
+  limitChar(date, 8);
+});
+
+
