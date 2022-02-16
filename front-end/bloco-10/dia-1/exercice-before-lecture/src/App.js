ae674eadc93f6e0c6e865b0fddce2b118fcbdb12
Christian Mendes Pereira <christianp3m@gmail.com>
2022-02-16 18:09:08 -0300

    Day 10.1 before lecture

---
+++ front-end/bloco-10/dia-1/exercice-before-lecture/src/App.js
@@ -0,0 +1,11 @@
+import Component from './Component';
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+  return <Component />
+  }
+}
+
+export default App;
---
+++ front-end/bloco-10/dia-1/exercice-before-lecture/src/Component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className='center'>
+          Christian Mendes Pereira
+        </h1>
+        <p className='center'>Já finalizei o módulo de Fundamentos. Agora estou no módulo de Front end, começando com React</p>
+      </div>
+    )
+  }
+}
+
+export default Component;
