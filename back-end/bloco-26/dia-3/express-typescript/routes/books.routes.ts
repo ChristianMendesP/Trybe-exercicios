33f18a3b433ac3354ab5988a1aaf794ee7080d25
Christian Mendes Pereira <christianp3m@gmail.com>
2022-08-03 15:10:07 -0300

    Prepare for block 27

---
+++ back-end/bloco-26/dia-3/express-typescript/routes/books.routes.ts
@@ -0,0 +1,19 @@
+// ./routes/books.routes.ts
+
+import { Router } from 'express';
+import BooksController from '../controllers/books.controller';
+import validationBook from '../middlewares/books.middleware';
+
+const router = Router();
+
+const booksSlashId = '/books/:id';
+
+const booksController = new BooksController();
+
+router.get('/books', booksController.getAll);
+router.get(booksSlashId, booksController.getById);
+router.post('/books/', validationBook, booksController.create);
+router.put(booksSlashId, validationBook, booksController.update);
+router.delete(booksSlashId, booksController.remove);
+
+export default router;
